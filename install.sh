31273f9ac83d193ec7ef94d2d7b5aba8bf589c54
Dat Vu <dat.vt@zodinet.com>
2021-08-17 15:48:43 +0700

    env

--- install.sh
+++ install.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 
 # install dependencies
-sudo apt install -y python3-pip virtualenv tesseract-ocr poppler-utils libxext-dev libsm-dev libxrender-dev
+sudo apt install -y tesseract-ocr poppler-utils libxext-dev libsm-dev libxrender-dev
 
 # create virtual environment
 virtualenv env -p python3
 source env/bin/activate
 pip install .
-pip install google-cloud-vision
